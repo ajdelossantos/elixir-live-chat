e1da8ed7a8d9f804994e9d6eaa7d114a0b9e1ea1
Alvin James T. Delos Santos <alvin.delossantos@rstor.io>
2019-08-28 08:54:15 -0600

    feat: setup live view

--- config/config.exs
+++ config/config.exs
@@ -12,7 +12,10 @@ config :live_chat, LiveChatWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "mkHksC6cZKrlpuoFzqiJdxIOwFlxD+JQOLkL9NNV2/gOPThxtuUBE3DaAEUrs2N3",
   render_errors: [view: LiveChatWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: LiveChat.PubSub, adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: LiveChat.PubSub, adapter: Phoenix.PubSub.PG2],
+  live_view: [
+    signing_salt: "AQyWgLvrWTRJTIYTQrz4Y2xi+fQXDWIM"
+  ]
 
 # Configures Elixir's Logger
 config :logger, :console,
